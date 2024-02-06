8d352221a451a7d15a3dddac0344e4eafa483693
armandpl <adpl33@gmail.com>
2024-02-06 14:05:51 +0100

    add twohot dist

--- minidream/dist.py
+++ minidream/dist.py
@@ -1,5 +1,10 @@
-import torch.distributions as torchd
+from typing import Callable
+
 import torch
+import torch.distributions as torchd
+from torch import Tensor
+
+from minidream.functional import symexp, symlog
 
 
 # https://github.com/NM512/dreamerv3-torch/blob/2c7a81a0e2f5f0c7659ba73b0ddbedf2a7e2ecf4/tools.py#L425
@@ -28,3 +33,59 @@ class OneHotDist(torchd.one_hot_categorical.OneHotCategorical):
             probs = probs[None]
         sample += probs - probs.detach()
         return sample
+
+
+# https://github.com/Eclectic-Sheep/sheeprl/blob/033ad744e6a87bed1203453867ecaab81026c616/sheeprl/utils/distribution.py#L224
+class TwoHotEncodingDistribution:
+    def __init__(
+        self,
+        logits: Tensor,
+        dims: int = 0,
+        low: int = -20,
+        high: int = 20,
+        transfwd: Callable[[Tensor], Tensor] = symlog,
+        transbwd: Callable[[Tensor], Tensor] = symexp,
+    ):
+        self.logits = logits
+        self.probs = torch.nn.functional.softmax(logits, dim=-1)
+        self.dims = tuple(-x for x in range(1, dims + 1))
+        self.bins = torch.linspace(low, high, logits.shape[-1], device=logits.device)
+        self.low = low
+        self.high = high
+        self.transfwd = transfwd
+        self.transbwd = transbwd
+        self._batch_shape = logits.shape[: len(logits.shape) - dims]
+        self._event_shape = logits.shape[len(logits.shape) - dims : -1] + (1,)
+
+    @property
+    def mean(self) -> Tensor:
+        return self.transbwd((self.probs * self.bins).sum(dim=self.dims, keepdim=True))
+
+    @property
+    def mode(self) -> Tensor:
+        return self.transbwd((self.probs * self.bins).sum(dim=self.dims, keepdim=True))
+
+    def log_prob(self, x: Tensor) -> Tensor:
+        x = self.transfwd(x)
+        # below in [-1, len(self.bins) - 1]
+        below = (self.bins <= x).type(torch.int32).sum(dim=-1, keepdim=True) - 1
+        # above in [0, len(self.bins)]
+        above = below + 1
+
+        # above in [0, len(self.bins) - 1]
+        above = torch.minimum(above, torch.full_like(above, len(self.bins) - 1))
+        # below in [0, len(self.bins) - 1]
+        below = torch.maximum(below, torch.zeros_like(below))
+
+        equal = below == above
+        dist_to_below = torch.where(equal, 1, torch.abs(self.bins[below] - x))
+        dist_to_above = torch.where(equal, 1, torch.abs(self.bins[above] - x))
+        total = dist_to_below + dist_to_above
+        weight_below = dist_to_above / total
+        weight_above = dist_to_below / total
+        target = (
+            torch.nn.functional.one_hot(below, len(self.bins)) * weight_below[..., None]
+            + torch.nn.functional.one_hot(above, len(self.bins)) * weight_above[..., None]
+        ).squeeze(-2)
+        log_pred = self.logits - torch.logsumexp(self.logits, dim=-1, keepdims=True)
+        return (target * log_pred).sum(dim=self.dims)
