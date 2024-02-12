165e8c75a1f4583a0ce114d23d5bf348d378c651
Kishimisu <kishimisu.art@gmail.com>
2024-02-12 14:34:40 +0100

    sorry

---
+++ sorry.py
@@ -0,0 +1,6 @@
+from please  import je
+from forgive import suis
+from me      import désolé
+
+# Coucou, je suis tres chaud pour avancer sur ce projet avec toi 
+# si c'est toujours d'actualité et que tu ne m'as pas bloqué 😭
