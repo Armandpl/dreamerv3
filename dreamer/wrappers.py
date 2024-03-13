c8567399d2280269da279d4700d499269f1e251f
armandpl <adpl33@gmail.com>
2024-03-13 14:31:08 +0100

    try matching danijar's wrapper frameskipping strategy

--- dreamer/wrappers.py
+++ dreamer/wrappers.py
@@ -25,8 +25,13 @@ class PreProcessMinatar(gym.Wrapper):
 
 
 class PreProcessAtari(gym.Wrapper):
-    """Transpose obs to have channel first, resize, convert from int to float and rescale to [-1,
-    1]"""
+    """The wrapper expects to receive an atari env with frameskip=1 (no frame skipping).
+
+    It then repeats the same action for 4 frames and take the maximum of the last two frames. The
+    frame is then transposed to (c, h, w), resized to (64, 64), converted from int to float and
+    rescaled to [-1, 1] The reward is accumulated over the 4 frames. The terminated and truncated
+    flags are set to True if any of the 4 frames are terminated or truncated.
+    """
 
     def __init__(self, env: gym.Env):
         super().__init__(env)
@@ -38,13 +43,28 @@ class PreProcessAtari(gym.Wrapper):
         )
 
     def step(self, action):
-        obs, reward, terminated, truncated, info = self.env.step(action)
+        total_rew = 0.0
+        glob_terminated = False
+        glob_truncated = False
+
+        img_1, img_2 = None, None
+        for i in range(4):
+            obs, reward, terminated, truncated, _ = self.env.step(action)
+            total_rew += reward
+            glob_terminated = glob_terminated or terminated
+            glob_truncated = glob_truncated or truncated
+            if i == 2:
+                img_1 = obs
+            elif i == 3:
+                img_2 = obs
+
+        img = np.maximum(img_1, img_2)
 
         # TODO torch convert_image_dtype does more than / 255.0
         # do we need to worry about it?
         # using cv2 instead so that obs stays a numpy array, for consistency across envs
-        obs = cv2.resize(obs, (64, 64), interpolation=cv2.INTER_AREA)
+        obs = cv2.resize(img, (64, 64), interpolation=cv2.INTER_AREA)
         obs = obs.transpose(2, 0, 1).astype(np.float32) / 255.0
         obs = (obs * 2) - 1
 
-        return obs, reward, terminated, truncated, info
+        return obs, total_rew, glob_terminated, glob_truncated, {}
